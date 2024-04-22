fd59fb834c5ca1608068c066c960ed666a2c9a6b
ampil <33726853+ampil@users.noreply.github.com>
2024-04-22 11:32:56 +0300

    Update export-android-tabs.ps1
    + updated download cmdlet: instead of `wget` dependency, it uses native `Invoke-WebRequest`
    + the use of `Invoke-WebRequest` fixes the `wget` error
    + added `adb` validation: when adb.exe is not present where it should be, the script downloads it from Google and unpacks to a default location on the C:\ drive
    + added `output folder` validation: if it's not present, the script creates it
    + added `USB debugging` validation: the script shows a message with error and stops further execution if USB debugging is not enabled (or no connected devices available)
    + added adb server clean up - it releases port forwarding from Android devices after script execution
    + suppressed output from service commands, added custom messages

--- export-android-tabs.ps1
+++ export-android-tabs.ps1
@@ -1,43 +1,114 @@
 # run in PowerShell while a phone is connected and Chrome is open on the phone
 
-# based on the SO anwsers:
-# get a list of tabs:    https://android.stackexchange.com/a/199496
+# based on these SO anwsers
+# get a list of tabs  :  https://android.stackexchange.com/a/199496
 # convert JSON to CSV :  https://stackoverflow.com/a/43595214
 # with proper encoding:  https://stackoverflow.com/a/66136255
 
 
 # prereqiusites: 
-# 1) get the ADB - you can do that by downloading and extracting the package from Google [5.84 MB]:  https://developer.android.com/studio/releases/platform-tools
-# 2) USB debugging enabled on your device (no root required)
+# 1) USB debugging enabled on your device (no root required)
+# 2) [optional*] download the ADB app from Google and extract it [5.84 MB]:  https://developer.android.com/studio/releases/platform-tools
+#    *if it is missing the script will download and unpack it
 
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#STAGE 1 - PATH
-#to ADB folder
-$pathToADB = "C:\platform-tools"
-
-#to folder where to store tabs
-$pathToFiles = "E:\Clouds\OneDrive\Devices\11T"
-
-#name of the export file
-$nameTabs = "tabs"
-
-#full path to JSON and CSV
-$pathToJsonFile = $pathToFiles + "\" + $nameTabs + ".json"
-$pathToOutputFile = $pathToFiles + "\" + $nameTabs + ".csv"
+#set the folder where to store output files with tabs
+$outFolder = "C:\export\11T"
 
 
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#STAGE 2 - GET
-#get a list of tabs from the phone
-$pathToADB + ".\adb.exe forward tcp:9222 localabstract:chrome_devtools_remote"
-wget -O $pathToJsonFile http://localhost:9222/json/list
+function getAndroidTabs {
+	param(
+	 [Parameter(mandatory)]     [string]$outFolder
+	)
+	
+	
+	#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	#STAGE 1 - SET VARIABLES
+	$ErrorActionPreference = 'Stop'
+
+	#ADB 
+	$pathToADB = 'C:\platform-tools\adb.exe'
+
+	#name of the export files
+	$formattedDateTime = Get-Date -format "yyyy-MM-dd HH-mm-ss"
+	$nameTabs = 'tabs ' + $formattedDateTime
+
+	#full paths of JSON and CSV export files
+	$pathToJsonFile = $outFolder + "\" + $nameTabs + ".json"
+	$pathToOutputFile = $outFolder + "\" + $nameTabs + ".csv"
+
+	#forwarded link from Android device to this Windows device containing tabs in a JSON format
+	$port = '9223'
+	$url = "http://localhost:$port/json/list"
 
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#STAGE 3 - CONVERT
-#convert JSON to CSV with proper encoding for Cyrillic, German, French, etc.
-((Get-Content -Encoding utf8 -Path $pathToJsonFile -Raw) | ConvertFrom-Json) | Export-CSV $pathToOutputFile -NoTypeInformation -Encoding utf8
+	#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	#STAGE 2 - VALIDATION
+	#make sure ADB exists
+	If (!(Test-Path -Path $pathToADB)) {
+		Write-Warning "ADB is not found. Downloading it"
+		
+		# downloading
+		$urlADB = "https://dl.google.com/android/repository/platform-tools-latest-windows.zip"
+		$pathToDownloads = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+		Invoke-WebRequest -Uri $urlADB -OutFile "$pathToDownloads\platform-tools.zip"
+		
+		# extracting to a 2nd parent directory
+		Expand-Archive -Path "$pathToDownloads\platform-tools.zip" -DestinationPath (Split-path $pathToADB | split-path) -Force
+	}
 
+
+	# check if USB debugging enabled - read list of devices available to ADB
+	$cmdOutput = & $pathToADB devices 
+	
+	if ($cmdOutput[1].length -gt "device".length) {
+		Write-Host "USB debugging enabled. Accessing tabs..."
+	} else {
+		Write-Warning "USB debugging disabled or phone is not connected! Opeartion aborted. Please connect the phone and turn on USB debugging."
+		break
+	}
+
+	#make sure the folder to store data exists
+	If (!(Test-Path -Path $outFolder)) {
+		New-Item -ItemType Directory -Path $outFolder
+		Write-Host "Save directory created"
+	}
+
+
+
+	#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	#STAGE 3 - GET TABS FROM THE PHONE
+	#activate adb
+	Write-Host "Activating ADB port forwarding"
+	$params = "forward tcp:$port localabstract:chrome_devtools_remote"
+	$cmd = "& $pathToADB $params | Write-Verbose"   # with '| Write-Verbose' hide output of the command which is '9223'
+	Invoke-Expression $cmd
+
+	# download the list of tabs from the phone as a .json file
+	Invoke-WebRequest -Uri $url -OutFile $pathToJsonFile
+
+	#convert JSON to CSV with proper encoding for Cyrillic, German, French, etc.
+	if (!(Test-Path $pathToJsonFile)) {
+		Write-Warning "Tabs file ('$nameTabs.json') was not downloaded"
+	} else {
+		((Get-Content -Encoding utf8 -Path $pathToJsonFile -Raw) | ConvertFrom-Json) | Export-CSV $pathToOutputFile -NoTypeInformation -Encoding utf8
+		
+		Write-Host "Completed. Tabs exported to " $outFolder
+	}
+
+
+	#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	#STAGE 4 - CLEANING UP
+	# close adb server and free up the port
+	$params = "kill-server"
+	$cmd = "& $pathToADB $params"
+	Invoke-Expression $cmd
+
+}
+
+
+# call the function
+getAndroidTabs $outFolder
